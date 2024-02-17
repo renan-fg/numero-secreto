4e1cc581520a0456821fdd88529200c52beb8b76
Renan de França Gonçalves <renandefranca.g@gmail.com>
2024-02-16 21:40:29 -0300

    Aumentado para 100 números

--- img/JS Game_files/app.js
+++ img/JS Game_files/app.js
@@ -5,7 +5,7 @@
 // alert('Boas vindas ao jogo do número secreto')
 
 // while (chute != numeroSecreto) {
-//     let chute = prompt('Escolha um número entre 1 e 10')
+//     let chute = prompt('Escolha um número entre 1 e 100')
 //     if (chute == numeroSecreto) {
 //         break
 //     } else {
